2bcb17f119fd3cbfa4ff7f06ec3ba64654018850
paramagician <konnor5456@gmail.com>
2018-12-08 05:20:43 +0000

    Added pry, bundle install, added remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,23 @@
 class User < ApplicationRecord
+  before_create :add_remember_digest
   validates :name, presence: true
   validates :email, presence: true
   ## Will not be fully validating w/ email regexp etc
 
   has_secure_password # uses bcrypt
+
+  def self.create_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def add_remember_digest
+    self.remember_token = User.create_token
+    self.remember_digest = User.digest(remember_token)
+  end
 end
