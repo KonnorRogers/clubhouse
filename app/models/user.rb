3e258e24a96e436f2ae8d3024bd6d7ec0f6856bd
paramagician <konnor5456@gmail.com>
2018-12-08 00:12:18 +0000

    Fixed migration to have password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  ## Will not be fully validating w/ email regexp etc
+
+  has_secure_password # uses bcrypt
 end
