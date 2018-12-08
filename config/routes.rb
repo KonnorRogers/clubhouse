1fdd6bb359cc5871e42461970b4733ba13e670be
paramagician <konnor5456@gmail.com>
2018-12-08 00:41:06 +0000

    removed generation of sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'sessions_controller/new'
-  get 'sessions_controller/edit'
-  get 'sessions_controller/update'
-  get 'sessions_controller/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
